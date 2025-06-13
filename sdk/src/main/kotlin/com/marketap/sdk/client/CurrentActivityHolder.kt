3e05c85ca70920926c61ea1953ef7548925f1c90
seokwoo-marketap <seokwoo.choi@marketap.io>
2025-06-13 18:17:01 +0900

    CurrentActivityHolder 로그 추가

--- sdk/src/main/kotlin/com/marketap/sdk/client/CurrentActivityHolder.kt
+++ sdk/src/main/kotlin/com/marketap/sdk/client/CurrentActivityHolder.kt
@@ -10,11 +10,17 @@ class CurrentActivityHolder : Application.ActivityLifecycleCallbacks {
     private var currentActivity: Activity? = null
 
     private var reservedAction: ((Activity) -> Unit)? = null
+
+    init {
+        Log.d("CurrentActivityHolder", "CurrentActivityHolder initialized")
+    }
+
     fun useActivity(block: (Activity) -> Unit) {
+        Log.d("CurrentActivityHolder", "useActivity called")
         val activity = currentActivity
         if (activity != null && !activity.isFinishing && !activity.isDestroyed) {
             Log.d(
-                "MESSAGE",
+                "CurrentActivityHolder",
                 activity.packageName + " useActivity: " + activity.javaClass.simpleName
             )
             block(activity)
@@ -26,6 +32,7 @@ class CurrentActivityHolder : Application.ActivityLifecycleCallbacks {
     fun get(): Activity? = currentActivity
 
     override fun onActivityResumed(activity: Activity) {
+        Log.d("CurrentActivityHolder", "onActivityResumed: " + activity.javaClass.simpleName)
         reservedAction?.let {
             reservedAction = null
             it(activity)
@@ -33,7 +40,9 @@ class CurrentActivityHolder : Application.ActivityLifecycleCallbacks {
         currentActivity = activity
     }
 
-    override fun onActivityPaused(activity: Activity) {}
+    override fun onActivityPaused(activity: Activity) {
+        Log.d("CurrentActivityHolder", "onActivityPaused: " + activity.javaClass.simpleName)
+    }
 
     // 나머지는 무시
     override fun onActivityCreated(a: Activity, b: Bundle?) {}
