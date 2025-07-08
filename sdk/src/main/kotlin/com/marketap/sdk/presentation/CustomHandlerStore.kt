89333f70350cc34e90645e00ed2569c68266e6ee
seokwoo-marketap <seokwoo.choi@marketap.io>
2025-07-08 17:38:33 +0900

    커스텀 핸들러가 호출될 때, 액티비티 실행 보장

--- sdk/src/main/kotlin/com/marketap/sdk/presentation/CustomHandlerStore.kt
+++ sdk/src/main/kotlin/com/marketap/sdk/presentation/CustomHandlerStore.kt
@@ -2,6 +2,7 @@ package com.marketap.sdk.presentation
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import com.marketap.sdk.model.external.MarketapClickEvent
 import com.marketap.sdk.model.external.MarketapClickHandler
 import com.marketap.sdk.presentation.MarketapRegistry.marketapClickHandler
@@ -23,14 +24,20 @@ internal object CustomHandlerStore {
         val handler = marketapClickHandler
         return if (isCustomized(activity.applicationContext)) {
             logger.d { "Marketap SDK click handled by custom handler" }
+            if (activity.isTaskRoot) {
+                activity.packageManager.getLaunchIntentForPackage(activity.packageName)?.apply {
+                    addFlags(
+                        Intent.FLAG_ACTIVITY_REORDER_TO_FRONT or
+                                Intent.FLAG_ACTIVITY_NEW_TASK or
+                                Intent.FLAG_ACTIVITY_SINGLE_TOP
+                    )
+                }?.let {
+                    activity.startActivity(it)
+                }
+            }
             if (handler == null) {
                 logger.w { "Marketap SDK click handler is not set, pending click and launching app if task root" }
                 pendingClick = click
-                if (activity.isTaskRoot) {
-                    activity.packageManager.getLaunchIntentForPackage(activity.packageName)?.let {
-                        activity.startActivity(it)
-                    }
-                }
             } else {
                 try {
                     handler.handleClick(click)
